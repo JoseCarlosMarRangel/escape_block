3dd20056bb3b1ac48a29b52dd553a9f462124a97
Jose Carlos Mar Rangel <ghost@debian-BULLSEYE-live-builder-AMD64>
2022-01-21 00:25:59 -0600

    situciones actualizadas

--- situaciones.py
+++ situaciones.py
@@ -2,4 +2,14 @@ def buscar_valores(matriz):
         print("")
         print("Hola soy situaciones")
         print("")
-        print(matriz)     
+        print(matriz)
+        print("")
+        b = matriz[2][0]
+        print("Este es el valor del rey: " + str(b))
+        print("")
+
+        avance_valores(matriz)        
+
+def  avance_valores(matriz):
+        if matriz[2][1] == 0:
+                print("el valor de la casilla es matriz: " + matriz[2][1])
